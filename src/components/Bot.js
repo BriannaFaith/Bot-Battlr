8d341921036b2747d6e0e4e5b4e594912974de5f
FaithOgendi <faithbrianna01@gmail.com>
2024-02-11 17:04:13 +0300

    list bots as cards, added styling

--- src/components/Bot.js
+++ src/components/Bot.js
@@ -1,18 +1,19 @@
 import React from "react";
+import "../App.css"
 
 function Bot({ bot }) {
   const { id, name, health, damage, armor, bot_class, catchphrase, avatar_url } = bot;
 
   return (
     <div className="bot-card">
-      <img classname="bot-avatar" src={avatar_url} alt={name} />
-      <div classname= "bot-details">
-      <h3>{name}</h3>
-      <p>Class: {bot_class}</p>
-      <p>Health: {health}</p>
-      <p>Damage: {damage}</p>
-      <p>Armor: {armor}</p>
-      <p>Catchphrase: {catchphrase}</p>
+      <img className="bot-avatar" src={avatar_url} alt={name} />
+      <div className="bot-details">
+        <h3>{name}</h3>
+        <p>Class: {bot_class}</p>
+        <p>Health: {health}</p>
+        <p>Damage: {damage}</p>
+        <p>Armor: {armor}</p>
+        <p>Catchphrase: {catchphrase}</p>
       </div>
       <button>Enlist</button>
     </div>
